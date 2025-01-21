07d1a2ad9925f208ad8b8ad2e968f7b22dffffab
Aidan Dunsmore <bi23zv@student.sunderland.ac.uk>
2025-01-21 15:13:00 +0000

    Added full list of countries with 4 fields of info

--- countriesAPI-sqlite.js
+++ countriesAPI-sqlite.js
@@ -11,6 +11,41 @@ db.serialize(function(){
     db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Germany", "Europe", "84,480,000", "Black,Red,Yellow");
 
     db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Peru", "South America", "34,350,000", "Red,White");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Wales", "Europe", "3,350,000", "Red,White,Green");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Japan", "Asia", "124,500,000", "Red,White");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Australia", "Oceania", "26,64,000", "Red,White,Blue");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Armenia", "Asia", "2,778,000", "Red,Blue,Orange");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Russia", "Europe", "143,350,000", "Red,White,Blue");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Canada", "North America", "40,350,000", "Red,White");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Gabon", "Africa", "2,350,000", "Green,Yellow,Blue");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Yemen", "Asia", "34,450,000", "Red,White,Black");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Argentina", "South America", "46,650,000", "Blue,White,Yellow");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Chile", "South America", "19,630,000", "Red,White,Blue");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "England", "Europe", "59,350,000", "Red,White");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Nigeria", "Africa", "223,800,000", "Green,White");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Kazakhstan", "Asia", "19,950,000", "Blue,Yellow");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Samoa", "Oceania", "225,681", "Red,White,Blue");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Estonia", "Europe", "1,366,000", "Blue,Black,White");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Jamaica", "North America", "2,826,000", "Green,Yellow,Black");
+
+    db.run("INSERT INTO countries (name, continent, population, flag) VALUES (?,?,?,?)", "Indonesia", "Asia", "277,500,000", "Red,White");
+
 });
 
 var express = require('express');
@@ -18,6 +53,12 @@ var app = express();
 
 app.get('/countries', function(req,res){
     db.all("SELECT * FROM countries", function(err,rows){
+        res.jsonp(rows);
+    });
+});
+
+app.get('/flagcolour', function(req,res){
+    db.all("SELECT * FROM countries WHERE flag LIKE '%White%'", function(err,rows){
         res.jsonp(rows);
     });
 });
